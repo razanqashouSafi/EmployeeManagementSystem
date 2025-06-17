204cda8ab1b3d3e8396a8e5472b56645127faded
razanqashou <razanqashou777@gmail.com>
2025-06-17 10:15:35 +0300

    add employee to db

--- EmployeeManagementSystem/Pages/EmployeeFE/AddEmployee.cshtml.cs
+++ EmployeeManagementSystem/Pages/EmployeeFE/AddEmployee.cshtml.cs
@@ -1,23 +1,40 @@
-using EmployeeManagementSystem.Context;
+﻿using EmployeeManagementSystem.Context;
 using EmployeeManagementSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeManagementSystem.Pages.EmployeeFE
 {
     public class AddEmployeeModel : PageModel
     {
 
-        private readonly EmployeeManagementDbContextb context;
+        private readonly EmployeeManagementDbContextb _context;
         public AddEmployeeModel(EmployeeManagementDbContextb context)
         {
-            this.context = context;
+           _context = context;
         }
 
         [BindProperty]
         public Employee Employee { get; set; }
         public void OnGet()
         {
+        }
+
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();  
+            }
+
+            Employee.CreateDate = DateTime.Now;
+
+            _context.Employees.Add(Employee);
+            _context.SaveChanges();
+
+            return RedirectToPage("/Index"); 
         }
     }
 }
